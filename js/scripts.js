bfb171df98612b9d713096109d8ff963c6ee8ef2
ritabc <ritabc@gmail.com>
2018-06-01 13:26:25 -0700

    Tally number answers for each track

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,39 @@
-function countRuby() {
+function tally() {
   var rubyTally = 0;
+  var cSharpTally = 0;
+  var cssTally = 0;
+  var tallies = [];
   var questions = $(".radio");
 
-  // loop through questions and return value of checked answer
+  // loop through questions and log value of checked answer
   for (i = 1; i < questions.length + 1; ++i) {
     questionNumber = "question" + i;
-    //
-    test = $("input[name=" + questionNumber + "]:checked").val();
-    console.log(test);
-  };
+    var answer = $("input[name=" + questionNumber + "]:checked").val();
+    if (answer === "ruby") {
+      ++rubyTally;
+    }
+    else if (answer === "c-sharp") {
+      ++cSharpTally;
+    }
+    else if (answer === "css") {
+      ++cssTally;
+    }
+    else {
+      return "Something went wrong"
+    }
+  }
+
+  tallies.push(rubyTally);
+  tallies.push(cSharpTally);
+  tallies.push(cssTally);
+  console.log(tallies)
+  return tallies;
 }
 
 $(document).ready(function(){
   $("#submit").click(function(e){
     e.preventDefault();
-    countRuby();
-
+    tally();
   });
 
 
