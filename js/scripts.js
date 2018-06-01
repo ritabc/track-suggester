a644cfd053a01f074c4e2238ffab4cf75f97a682
ritabc <ritabc@gmail.com>
2018-06-01 13:10:23 -0700

    Add functionality to return value of question 1 answer

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 function countRuby() {
   var rubyTally = 0;
-  var questions = $(".radio");
+  var questions = $(".radio"); 
 
-  // loop through questions and increment tally variable if val="ruby"
+  // loop through question1 answers and return value of checked answer
   for (i = 1; i < questions.length + 1; ++i) {
     questionNumber = "question" + i;
-    test = $([type="radio"]:checked);
+    //
+    test = $("input[name=question1]:checked", "#quiz").val();
     console.log(test);
   };
 }
